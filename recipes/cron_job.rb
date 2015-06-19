73b920515fa76c638f064e49632e18847ee596a1
Kyle Zarazan <zarazan@gmail.com>
2015-06-19 12:30:02 -0500

    specify user and minute in cron job

--- recipes/cron_job.rb
+++ recipes/cron_job.rb
@@ -1,5 +1,7 @@
 cron "product_report" do
-  path "/usr/local/bin:$PATH"
-  hour "1"
-  command "cd /srv/www/dailydeeds/current && rake RAILS_ENV=production automated:send_payment_report"
+  path '/usr/local/bin:$PATH'
+  minute '0'
+  hour '1'
+  user 'deploy'
+  command "cd /srv/www/dailydeeds/current && bundle exec rake RAILS_ENV=production automated:send_payment_report"
 end
